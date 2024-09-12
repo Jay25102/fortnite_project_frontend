52158f940aea5fa41a56b62189c307dbda1bcc03
Jay25102 <jasonhuang25102@gmail.com>
2024-09-12 00:28:06 -0400

    fixing merge mistake

--- server.js
+++
@@ -1,9 +0,0 @@
-// import app from "./app.js";
-// import { PORT } from "./config.js";
-
-const app = require("./app");
-const { PORT } = require("./config");
-
-app.listen(PORT, function() {
-    console.log(`Started on http://localhost:${PORT}`);
-})
